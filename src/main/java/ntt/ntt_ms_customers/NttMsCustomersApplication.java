28e54d3dd62e4302f8a87b2b136f994bd9edbb3b
Fabian216 <fabian.alan.olaya@gmail.com>
2025-09-23 17:58:21 -0500

    actualizando eurekaClient

--- src/main/java/ntt/ntt_ms_customers/NttMsCustomersApplication.java
+++ src/main/java/ntt/ntt_ms_customers/NttMsCustomersApplication.java
@@ -2,8 +2,10 @@ package ntt.ntt_ms_customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class NttMsCustomersApplication {
 
 	public static void main(String[] args) {
